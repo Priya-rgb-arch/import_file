c657b31cf2cddf8b70e287092b995de0f0e3efb4
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-13 16:30:50 +0530

    Update Job25.java

--- src/Job25.java
+++ src/Job25.java
@@ -4,21 +4,43 @@ public class Job25 {
 
 	public static void main(String[] args) {
 		try {
-        	String scriptPath = "/home/udayvarma/Batch-jobs/Batch-job-25/scripts/getHMS.sh";
-        	
+
+ 
+
+            String scriptPath = "/home/sriharipriya/import_file/scripts/getHMS.sh";
+
+ 
+
+            // Create the ProcessBuilder for the shell script
+
+ 
+
             ProcessBuilder processBuilder = new ProcessBuilder("bash", scriptPath);
-            
-            
+
+ 
+
+            // Start the process
             Process process = processBuilder.start();
-            
-           
+
+ 
+
+            // Read the output from the process
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+ 
+
+            // Wait for the process to complete
             int exitCode = process.waitFor();
 
-            if (exitCode == 0) {
-                System.out.println(" successfully.");
-            } else {
-                System.out.println("Failed .");
-            }
+ 
+
+            // Print the exit code
+            System.out.println("Script execution completed with exit code: " + exitCode);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
