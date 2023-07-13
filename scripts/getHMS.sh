3bfded6b3959cbf95a3569e98bd634174c352b9f
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-13 16:09:55 +0530

    Update getHMS.sh

--- scripts/getHMS.sh
+++ scripts/getHMS.sh
@@ -2,8 +2,8 @@
 
 echo "Script is start"
 
-awk -F',' '{print $0}' FDB_Daily_price_updates.csv
+#awk -F',' '{print $0}' FDB_Daily_price_updates.csv
 
 #source /mnt/c/Users/AMBATS/Desktop/Batchjob/SourceFile/FDB_Daily_price_updatesnew.csv
 
-echo "Script is running"
+#echo "Script is running"
