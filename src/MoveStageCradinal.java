b1ce0e0bcf9968f964e489f2b3b26183be69a960
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-14 14:46:20 +0530

    Add files via upload

---
+++ src/MoveStageCradinal.java
@@ -0,0 +1,36 @@
+package com.stage.move;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.*;
+
+public class MoveStageCradinal {
+
+	public static void main(String[] args) {
+		try {
+			String scriptPath = "/home/sriharipriya/MoveStageCardinal/MoveStageCardinals/src/stageCradinal.sh";
+
+			// Create the ProcessBuilder for the shell script
+
+			ProcessBuilder processBuilder = new ProcessBuilder("bash", scriptPath);
+
+			// Start the process
+
+			Process process = processBuilder.start();
+
+			// Wait for the process to complete
+			int exitCode = process.waitFor();
+
+			// Handle the exit code
+			if (exitCode == 0) {
+				System.out.println(
+						"Cardinal cost file move to source directory and Cost changes pushed to the store database successfully.");
+			} else {
+				System.out.println("Failed. Exit code: " + exitCode);
+			}
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
